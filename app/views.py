d3b6a002cd96613efe9d893c2615fa4fd24a14a5
Ackeem Smith <ackeem.smith@mymona.uwi.edu>
2019-02-11 04:07:38 +0000

    My prfile

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
+import datetime
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
@@ -22,7 +22,28 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Ackeem Smith")
+    
+
+@app.route('/profile')
+def profile():
+    userData = {
+        'profile_url': url_for('static', filename='images/img.jpg'),
+        'fullname': 'Ackeem Smith',
+        'username': 'pryncefreshie',
+        'location': 'Kingston, Jamaica',
+        'join_date': format_date_joined(),
+        's_bio': "I am a HUGE Game of Thrones fan!",
+        'no_of_posts': '45',
+        'no_of_followers': '1500',
+        'no_following': '50'
+    }
+    
+    return render_template('profile.html', userdata = userData);
+
+def format_date_joined():
+    return datetime.date(201,2,10).strftime("%B, %Y")
+    
 
 
 ###
